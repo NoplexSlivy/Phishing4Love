652c1388de7f81b937d79bc866f8c525868dd4e4
NoplexSlivy <134464820+NoplexSlivy@users.noreply.github.com>
2025-03-22 17:08:07 -0500

    updatesto the python and a working html file. wlell oen towards what we want.

--- Phishing4Love/Phishing4Hearts.py
+++ Phishing4Love/Phishing4Hearts.py
@@ -1,12 +1,16 @@
 import flask
 from flask import render_template
 import requests
+import random as rand
 
-emails = {"uah.college@gmail.com": True, "Totallyfake@gmail.com": False, }
+
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("Phishing.html")
+    x = rand.choice( list(emails.keys()))
+    Age = rand.randint(18,68)
+    emails = {"uah.college@gmail.com": True,"apple@gmail.com": True, "Totallyfake@gmail.com": False, "To2389@uah.edu": False, "admin@uah.edu": False }
+    return render_template("Phishing.html", Age=Age, ranemail=x, emails=emails)
 app.run(host="0.0.0.0", port=80)
-
+ 
