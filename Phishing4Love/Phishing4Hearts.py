dd412b3320c72f68b2bf41040af6ebca56a3ce56
NoplexSlivy <134464820+NoplexSlivy@users.noreply.github.com>
2025-03-22 16:23:10 -0500

    Update Phishing4Hearts.py

--- Phishing4Love/Phishing4Hearts.py
+++ Phishing4Love/Phishing4Hearts.py
@@ -2,11 +2,11 @@ import flask
 from flask import render_template
 import requests
 
-emails = {"uah.college@gmail.com": True, "Totallyfake@gmail.com": False, }
+emails = {"uah.college@gmail.com": True,"apple@gmail.com": True, "Totallyfake@gmail.com": False, "To2389@uah.edu": False, "admin@uah.edu": False }
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("Phishing.html")
 app.run(host="0.0.0.0", port=80)
-
+ 
