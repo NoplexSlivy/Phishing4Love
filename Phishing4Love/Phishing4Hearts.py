3720bbe550c674388ba964d897c25198e51c9f72
NoplexSlivy <134464820+NoplexSlivy@users.noreply.github.com>
2025-03-22 23:10:52 -0500

    safe model

--- Phishing4Love/Phishing4Hearts.py
+++ Phishing4Love/Phishing4Hearts.py
@@ -9,8 +9,7 @@ app = flask.Flask(__name__)
 def index():
     emails = {"uah.college@gmail.com": True,"apple@gmail.com": True, "Totallyfake@gmail.com": False, "To2389@uah.edu": False, "admin@uah.edu": False }
     x = rand.choice( list(emails.keys()))
-    age = rand.randint(18, 100)
-    Age = str(age)
+    Age = rand.randint(18, 100)
     return render_template("Phishing.html", emails=emails, Age = Age)
 app.run(host="0.0.0.0", port=80)
  
