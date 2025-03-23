0dd17fbb629107b465f0e8deb91aea6f5e0327f9
NoplexSlivy <134464820+NoplexSlivy@users.noreply.github.com>
2025-03-22 22:55:31 -0500

    remove extra code

--- Phishing4Love/Phishing4Hearts.py
+++ Phishing4Love/Phishing4Hearts.py
@@ -9,10 +9,8 @@ app = flask.Flask(__name__)
 def index():
     emails = {"uah.college@gmail.com": True,"apple@gmail.com": True, "Totallyfake@gmail.com": False, "To2389@uah.edu": False, "admin@uah.edu": False }
     x = rand.choice( list(emails.keys()))
-    def y():
-        Age = rand.randint(18,68)
-        return Age
-    
-    return render_template("Phishing.html", Age=y(), ranemail=x, emails=emails)
+    age = rand.randint(18, 100)
+    Age = str(age)
+    return render_template("Phishing.html", emails=emails, Age = Age)
 app.run(host="0.0.0.0", port=80)
  
